4f2d4d15a0ed6940d2cf55038fe0b5fe60e6c359
a55663340 <a6232241@g.pccu.edu.tw>
2020-09-19 21:37:43 +0800

    修改 Header Menu、並新增 carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,78 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
+import { Nav, Container, Row, Col, Carousel } from 'react-bootstrap'
+import logo from '../assets/image/logo.png'
+import bg from '../assets/image/bg.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
-      <Nav
-        className='justify-content-center text-center bg-dark'
-        style={{ fontWeight: '900', fontSize: '2rem' }}
-      >
-        <Nav.Item className='col-sm-12 col-md-auto'>
-          <Nav.Link href='/' className='text-light'>
-            主頁
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item className='col-sm-12 col-md-auto'>
-          <Nav.Link href='/aboutus' className='text-light'>
-            關於我們
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item className='col-sm-12 col-md-auto'>
-          <Nav.Link href='/introduction' className='text-light'>
-            故事大綱
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item className='col-sm-12 col-md-auto'>
-          <Nav.Link href='/operating' className='text-light'>
-            操作玩法
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <Container fluid>
+        <Row className='justify-content-center'>
+          <Carousel className='mw-100 h-50 d-inline-block'>
+            <Carousel.Item>
+              <img className='d-block w-100' src={bg} alt='First slide' />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className='d-block w-100' src={bg} alt='First slide' />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className='d-block w-100' src={bg} alt='First slide' />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Row>
+        <Row className='bg-dark'>
+          <Col md={2} className='px-5 align-self-center'>
+            <Link to='/'>
+              <img src={logo} alt='img' className='mw-100' />
+            </Link>
+          </Col>
+          <Col md={8} className='align-self-center'>
+            <Nav
+              className='justify-content-center text-center'
+              style={{ fontWeight: '900', fontSize: '1.5rem' }}
+            >
+              <Nav.Item className='col-sm-12 col-md-auto'>
+                <Nav.Link href='/' className='text-light'>
+                  主頁
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className='col-sm-12 col-md-auto'>
+                <Nav.Link href='/aboutus' className='text-light'>
+                  關於我們
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className='col-sm-12 col-md-auto'>
+                <Nav.Link href='/introduction' className='text-light'>
+                  故事大綱
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item className='col-sm-12 col-md-auto'>
+                <Nav.Link href='/operating' className='text-light'>
+                  操作玩法
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
     </header>
   )
 }
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import bg from '../assets/image/bg.png'
-import logo from '../assets/image/logo.png'
 
 const Index = () => {
   return (
     <Container>
-      <img src={bg} alt='bg' />
     </Container>
   )
 }
