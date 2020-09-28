b2c1d4001f4fdbf258a36949551e383752e9471c
kuan cheng <a6232241@g.pccu.edu.tw>
2020-09-28 21:01:44 +0800

    修改 title

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { renderRoutes } from 'react-router-config'
 import routes from '../router'
 import '../assets/css/index.sass'
 
-const App = () => (
-  <div className='App text-light'>
-    <Header />
-    <main className='py-3'>{renderRoutes(routes)}</main>
-    <Footer />
-  </div>
-)
+const App = () => {
+  useEffect(() => {
+    for (let i of routes) {
+      if (window.location.pathname === i.path) {
+        document.title = `${i.title}`
+      }
+    }
+  })
+
+  return (
+    <div className='App text-light'>
+      <Header />
+      <main className='py-3'>{renderRoutes(routes)}</main>
+      <Footer />
+    </div>
+  )
+}
 
 export default App
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 
 const Index = () => {
+
   return (
     <Container>
     </Container>
