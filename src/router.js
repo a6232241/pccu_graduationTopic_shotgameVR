daea046d8fa78b636a9949299db1a7f50b0e8d0b
a55663340 <a6232241@g.pccu.edu.tw>
2020-10-11 20:34:39 +0800

    故事大綱修改為敵人&槍枝、操作方法改為闖關流程，並移除關於我們和新增闖關流程的內容

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import Index from './pages/Index'
-import AboutUs from './pages/AboutUs'
+// import AboutUs from './pages/AboutUs'
 import Introduction from './pages/Introduction'
 import Operating from './pages/Operating'
 import NoFound from './pages/NoFound'
@@ -12,27 +12,27 @@ const routes = [
     name: 'index',
     title: '主頁',
   },
-  {
-    path: '/aboutus',
-    component: AboutUs,
-    name: 'aboutus',
-    title: '關於我們',
-  },
+  // {
+  //   path: '/aboutus',
+  //   component: AboutUs,
+  //   name: 'aboutus',
+  //   title: '關於我們',
+  // },
   {
     path: '/introduction',
     component: Introduction,
     name: 'introduction',
-    title: '故事大綱',
+    title: '敵人&槍枝',
   },
   {
     path: '/operating',
     component: Operating,
     name: 'operating',
-    title: '操作玩法',
+    title: '闖關流程',
   },
   {
-    component: NoFound
-  }
+    component: NoFound,
+  },
 ]
 
 export default routes
